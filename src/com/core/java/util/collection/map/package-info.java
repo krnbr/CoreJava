f08d7c3b8d580fb51a2ef2a9b227dc4c2398f818
Karanbir Singh <krnbr@live.in>
2014-09-10 21:20:16 +0530

    Package for Set and Maps added
    Package for Set and Maps added

---
+++ src/com/core/java/util/collection/map/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author suraj
+ * Collections framework Map related classes are found in this package
+ */
+package com.core.java.util.collection.map;
---
+++ src/com/core/java/util/collection/set/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author suraj
+ * Collections framework Set related classes are found in this package
+ */
+package com.core.java.util.collection.set;
